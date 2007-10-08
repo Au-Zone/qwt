f0a6a81e133f3eee1a4254da817f84abe052dee7
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2007-10-08 17:57:52 +0000

    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk@188 29e17f8d-cf37-471e-a288-72cc03a76eda

--- qwt/src/qwt_paint_cache.h
+++ qwt/src/qwt_paint_cache.h
@@ -7,8 +7,8 @@
  * modify it under the terms of the Qwt License, Version 1.0
  *****************************************************************************/
 
-#ifndef QWT_PAINT_BUFFER_H
-#define QWT_PAINT_BUFFER_H 1
+#ifndef QWT_PAINT_CACHE_H
+#define QWT_PAINT_CACHE_H 1
 
 #include "qwt_global.h"
 
@@ -16,26 +16,35 @@ class QPainter;
 class QPixmap;
 class QWidget;
 
+/*!
+  \brief Abstract buffer for paint operations
+
+  QwtPaintCache provides an common API for using a QPixmap or
+  QGLPixelBuffer as paint cache. It is intended to assist the 
+  implementation of a OpenGL and "regular" plot canvas, but could
+  be useful for similar situations.
+
+  \sa QwtPlotCanvas, QwtPixmapPaintCache
+*/
+
 class QWT_EXPORT QwtPaintCache
 {
 public:
     virtual ~QwtPaintCache();
 
-    virtual void init() = 0;
-    virtual void reset() = 0;
-    virtual void fill() = 0;
+    virtual void setEnabled(bool on) = 0;
+
+    virtual void sync(bool doFill) = 0;
     virtual void flush(QPainter *) = 0;
 
     virtual void invalidate() = 0;
+    bool isValid() const;
 
     QPaintDevice *buffer();
     const QPaintDevice *buffer() const;
 
     QWidget *widget();
     const QWidget *widget() const;
-
-    void clear();
-    bool isValid() const;
 
 protected:
     explicit QwtPaintCache(QWidget *widget);
@@ -52,12 +61,15 @@ public:
     QwtPixmapPaintCache(QWidget *);
     virtual ~QwtPixmapPaintCache();
 
-    virtual void init();
-    virtual void reset();
-    virtual void fill();
+    virtual void setEnabled(bool on);
+
+    virtual void sync(bool doFill);
     virtual void flush(QPainter *);
 
     virtual void invalidate();
+
+private:
+    QPixmap *initPixmap() const;
 };
 
 #endif
