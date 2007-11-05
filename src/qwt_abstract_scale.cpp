436bbc49f4c90094fe5525021332efeb3f6be20e
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2007-11-05 13:53:35 +0000

    git-svn-id: svn://svn.code.sf.net/p/qwt/code/branches/qwt-5.0@216 29e17f8d-cf37-471e-a288-72cc03a76eda

--- src/qwt_abstract_scale.cpp
+++ src/qwt_abstract_scale.cpp
@@ -226,6 +226,9 @@ void QwtAbstractScale::setAbstractScaleDraw(QwtAbstractScaleDraw *scaleDraw)
     if ( scaleDraw == NULL || scaleDraw == d_data->scaleDraw )
         return;
 
+    if ( d_data->scaleDraw != NULL )
+        scaleDraw->setScaleDiv(d_data->scaleDraw->scaleDiv());
+
     delete d_data->scaleDraw;
     d_data->scaleDraw = scaleDraw;
 } 
