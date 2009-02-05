11f6606648f6fffd08568dec60c75ad46cbe3628
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2009-02-05 14:40:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/qwt/code/branches/qwt-5.1@446 29e17f8d-cf37-471e-a288-72cc03a76eda

--- src/qwt_painter.cpp
+++ src/qwt_painter.cpp
@@ -497,7 +497,7 @@ void QwtPainter::drawPolyline(QPainter *painter, const QwtPolygon &pa)
     if ( deviceClipping )
         cpa = clip(cpa);
 
-#if QT_VERSION >= 0x040000
+#if QT_VERSION >= 0x040000 && QT_VERSION < 0x040400
     bool doSplit = false;
     if ( painter->paintEngine()->type() == QPaintEngine::Raster &&
         painter->pen().width() >= 2 )
