d723ba52f1e921dead5aeac1a7cbcdf2181fe25c
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2010-01-07 16:21:33 +0000

    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk@650 29e17f8d-cf37-471e-a288-72cc03a76eda

--- qwt/examples/errorbars/errorbars.cpp
+++ qwt/examples/errorbars/errorbars.cpp
@@ -10,7 +10,55 @@
 #include <qwt_symbol.h>
 #include <qwt_series_data.h>
 #include <qwt_text.h>
+#include <qwt_scale_draw.h>
+#include <qdatetime.h>
 #include "friedberg2007.h"
+#if 1
+#include <QDebug>
+#endif
+
+class Grid: public QwtPlotGrid
+{
+public:
+    Grid()
+    {
+        enableXMin(true);
+        setMajPen(QPen(Qt::white, 0, Qt::DotLine));
+        setMinPen(QPen(Qt::gray, 0 , Qt::DotLine));
+    }
+
+    virtual void updateScaleDiv(const QwtScaleDiv &xMap,
+        const QwtScaleDiv &yMap)
+    {
+        QwtValueList ticks[QwtScaleDiv::NTickTypes];
+        
+        ticks[QwtScaleDiv::MajorTick] = xMap.ticks(QwtScaleDiv::MediumTick);
+        ticks[QwtScaleDiv::MinorTick] = xMap.ticks(QwtScaleDiv::MinorTick);
+
+        QwtPlotGrid::updateScaleDiv(
+            QwtScaleDiv(xMap.lowerBound(), xMap.upperBound(), ticks),
+            yMap );
+    }
+};
+
+class YearScaleDraw: public QwtScaleDraw
+{
+public:
+    YearScaleDraw()
+    {
+        setTickLength(QwtScaleDiv::MajorTick, 0);
+        setTickLength(QwtScaleDiv::MinorTick, 0);
+        setTickLength(QwtScaleDiv::MediumTick, 6);
+
+        setLabelRotation(-90.0);
+        setLabelAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+    }
+
+    virtual QwtText label(double value) const
+    {
+        return QDate::longMonthName(int(value / 30) + 1);
+    }
+};
 
 class FriedbergPlot: public QwtPlot
 {
@@ -23,6 +71,8 @@ private:
 
     void insertErrorBars(const QwtArray<QwtIntervalSample>&,
         const QColor &color, bool showTube);
+
+    QwtScaleDiv yearScaleDiv() const;
 };
 
 FriedbergPlot::FriedbergPlot()
@@ -31,14 +81,13 @@ FriedbergPlot::FriedbergPlot()
     setCanvasBackground(QColor(Qt::darkGray));
 
     setAxisTitle(QwtPlot::xBottom, "2007");
-    setAxisScale(QwtPlot::xBottom, 0.0, 365.0);
+    setAxisScaleDiv(QwtPlot::xBottom, yearScaleDiv());
+    setAxisScaleDraw(QwtPlot::xBottom, new YearScaleDraw());
+
     setAxisTitle(QwtPlot::yLeft, 
         QString("Temperature [%1C]").arg(QChar(0x00B0)) );
     // grid
-    QwtPlotGrid *grid = new QwtPlotGrid;
-    grid->enableXMin(true);
-    grid->setMajPen(QPen(Qt::white, 0, Qt::DotLine));
-    grid->setMinPen(QPen(Qt::gray, 0 , Qt::DotLine));
+    QwtPlotGrid *grid = new Grid;
     grid->attach(this);
 
     insertLegend(new QwtLegend(), QwtPlot::RightLegend);
@@ -61,6 +110,29 @@ FriedbergPlot::FriedbergPlot()
     QwtPlotZoomer* zoomer = new QwtPlotZoomer(canvas());
     zoomer->setRubberBandPen(QColor(Qt::black));
     zoomer->setTrackerPen(QColor(Qt::black));
+}
+
+QwtScaleDiv FriedbergPlot::yearScaleDiv() const
+{
+    const int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    QwtValueList ticks[QwtScaleDiv::NTickTypes];
+
+    QwtValueList &mediumTicks = ticks[QwtScaleDiv::MediumTick];
+    mediumTicks += 0.0;
+    for ( uint i = 0; i < sizeof(days) / sizeof(days[0]); i++ )
+        mediumTicks += mediumTicks.last() + days[i];
+
+    QwtValueList &minorTicks = ticks[QwtScaleDiv::MinorTick];
+    for ( int i = 1; i <= 365; i += 7 )
+        minorTicks += i;
+
+    QwtValueList &majorTicks = ticks[QwtScaleDiv::MajorTick];
+    for ( int i = 0; i < 12; i++ )
+        majorTicks += i * 30 + 15;
+
+    QwtScaleDiv scaleDiv(mediumTicks.first(), mediumTicks.last() + 1, ticks);
+    return scaleDiv;
 }
 
 void FriedbergPlot::insertCurve(const QString& title, 
@@ -106,10 +178,12 @@ void FriedbergPlot::insertErrorBars(
         errorCurve->setCurveStyle(QwtPlotIntervalCurve::NoCurve);
     }
 
+#if 0
     QwtIntervalSymbol errorBar(QwtIntervalSymbol::Bar);
     errorBar.setWidth(7);
     errorBar.setPen(QPen(color));
     errorCurve->setSymbol(errorBar);
+#endif
 
     errorCurve->setSamples(samples);
     errorCurve->attach(this);
