790bab3bbfdf6fa4509e631eca9d804f425f40d3
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2010-10-16 07:18:06 +0000

    git-svn-id: svn://svn.code.sf.net/p/qwt/code/branches/qwt-5.2@859 29e17f8d-cf37-471e-a288-72cc03a76eda

--- src/qwt_global.h
+++ src/qwt_global.h
@@ -19,8 +19,8 @@
 
 // QWT_VERSION is (major << 16) + (minor << 8) + patch.
 
-#define QWT_VERSION       0x050201
-#define QWT_VERSION_STR   "5.2.1"
+#define QWT_VERSION       0x050202
+#define QWT_VERSION_STR   "5.2.2"
 
 #if defined(Q_WS_WIN) || defined(Q_WS_S60)
 
