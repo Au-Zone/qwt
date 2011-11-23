74a5aa870de529f594a4d904f98c39c637c70e26
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2011-11-23 15:33:16 +0000

    legends example
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@1142 29e17f8d-cf37-471e-a288-72cc03a76eda

--- examples/legends/plot.cpp
+++ examples/legends/plot.cpp
@@ -2,35 +2,100 @@
 #include <qwt_plot_legenditem.h>
 #include <qwt_legend.h>
 #include <qwt_plot_canvas.h>
+#include <qwt_plot_grid.h>
 #include "plot.h"
+
+class LegendItem: public QwtPlotLegendItem
+{
+public:
+    LegendItem()
+    {
+#if 1
+        setBackgroundMode( LegendBackground );
+        setBorderRadius( 10 );
+        setRenderHint( QwtPlotItem::RenderAntialiased );
+#else
+        setBackgroundMode( ItemBackground );
+#endif
+
+        setMaxColumns( 1 );
+        setAlignment( Qt::AlignRight | Qt::AlignVCenter );
+
+        initColors();
+    }
+
+    void initColors()
+    {
+        QColor color( Qt::white );
+
+        setTextPen( color );
+#if 1
+        setBorderPen( color );
+
+        QColor c( Qt::gray );
+        c.setAlpha( 200 );
+
+        setBackgroundBrush( c );
+#endif
+    }
+};
+
+class Curve: public QwtPlotCurve
+{
+public:
+    Curve()
+    {
+        setRenderHint( QwtPlotItem::RenderAntialiased );
+        initData();
+    }
+
+    void initData()
+    {
+        QVector<QPointF> points;
+
+        double y = qrand() % 1000;
+
+        for ( double x = 0.0; x <= 1000.0; x += 100.0 )
+        {
+            double off = qrand() % 200 - 100;
+            if ( y + off > 980.0 || y + off < 20.0 )
+                off = -off;
+
+            y += off;
+
+            points += QPointF( x, y );
+        }
+
+        setSamples( points );
+    }
+};
 
 Plot::Plot( QWidget *parent ):
     QwtPlot( parent )
 {
     canvas()->setFocusIndicator( QwtPlotCanvas::CanvasFocusIndicator );
     canvas()->setFocusPolicy( Qt::StrongFocus );
+    canvas()->setPalette( Qt::black );
 
     setAutoReplot( false );
 
     setTitle( "Legend Test" );
     setFooter( "Footer" );
 
+    // grid
+    QwtPlotGrid *grid = new QwtPlotGrid;
+    grid->enableXMin( true );
+    grid->setMajPen( QPen( Qt::gray, 0, Qt::DotLine ) );
+    grid->setMinPen( QPen( Qt::darkGray, 0 , Qt::DotLine ) );
+    grid->attach( this );
+
+    // legend
+    LegendItem *legendItem = new LegendItem();
+    legendItem->attach( this );
+
+    // curves
     for ( int i = 0; i < 4; i++ )
         insertCurve();
-
-	QColor c( Qt::white );
-
-	QwtPlotLegendItem *legendItem = new QwtPlotLegendItem();
-	legendItem->setSpan( 2 );
-	legendItem->setOrientation( Qt::Vertical );
-	legendItem->setBorderRadius( 5 );
-
-	c.setAlpha( 100 );
-	legendItem->setBorderPen( c );
-	c.setAlpha( 20 );
-	legendItem->setBackgroundBrush( c );
-	
-	legendItem->attach( this );
 
     setAutoReplot( true );
 }
@@ -42,7 +107,24 @@ void Plot::insertCurve()
     QString title("Curve %1");
     title = title.arg( counter ++ );
 
-    QwtPlotCurve *curve = new QwtPlotCurve( title );
+    const char *colors[] = 
+    { 
+        "LightSalmon",
+        "HotPink",
+        "Yellow",
+        "Fuchsia",
+        "PaleGreen",
+        "PaleTurquoise",
+        "SteelBlue",
+        "Cornsilk",
+        "Peru",
+        "Maroon"
+    };
+    const int numColors = sizeof( colors ) / sizeof( colors[0] );
+
+    QwtPlotCurve *curve = new Curve();
+    curve->setTitle( title );
+    curve->setPen( QPen( QColor( colors[ counter % numColors ] ), 2 ) );
     curve->attach( this );
 }
 
@@ -64,6 +146,5 @@ void Plot::insertLegend()
 
 void Plot::removeItem( QwtPlotItem *item )
 {
-qDebug() << "removeItem: " << item;
     delete item;
 }
