8478479799ceb20c1f495182aed6d4cac9f46626
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2012-03-03 09:58:24 +0000

    git-svn-id: svn://svn.code.sf.net/p/qwt/code/branches/qwt-5.2@1237 29e17f8d-cf37-471e-a288-72cc03a76eda

--- src/qwt_scale_div.cpp
+++ src/qwt_scale_div.cpp
@@ -154,7 +154,7 @@ void QwtScaleDiv::invert()
 */
 void QwtScaleDiv::setTicks(int type, const QwtValueList &ticks)
 {
-    if ( type >= 0 || type < NTickTypes )
+    if ( type >= 0 && type < NTickTypes )
         d_ticks[type] = ticks;
 }
 
@@ -165,7 +165,7 @@ void QwtScaleDiv::setTicks(int type, const QwtValueList &ticks)
 */
 const QwtValueList &QwtScaleDiv::ticks(int type) const
 {
-    if ( type >= 0 || type < NTickTypes )
+    if ( type >= 0 && type < NTickTypes )
         return d_ticks[type];
 
     static QwtValueList noTicks;
