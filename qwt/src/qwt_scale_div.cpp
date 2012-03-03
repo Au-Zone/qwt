0ab2dabab70a359714c6300beb224ca935915db3
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2012-03-03 09:56:51 +0000

    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk@1235 29e17f8d-cf37-471e-a288-72cc03a76eda

--- qwt/src/qwt_scale_div.cpp
+++ qwt/src/qwt_scale_div.cpp
@@ -141,7 +141,7 @@ void QwtScaleDiv::invert()
 */
 void QwtScaleDiv::setTicks( int type, const QList<double> &ticks )
 {
-    if ( type >= 0 || type < NTickTypes )
+    if ( type >= 0 && type < NTickTypes )
         d_ticks[type] = ticks;
 }
 
@@ -152,7 +152,7 @@ void QwtScaleDiv::setTicks( int type, const QList<double> &ticks )
 */
 const QList<double> &QwtScaleDiv::ticks( int type ) const
 {
-    if ( type >= 0 || type < NTickTypes )
+    if ( type >= 0 && type < NTickTypes )
         return d_ticks[type];
 
     static QList<double> noTicks;
