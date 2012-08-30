bea159e1d4db35f008cf761dc7d1c691f6fde037
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2012-08-30 09:27:28 +0000

    QwtPlotTextLabel introduced
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@1408 29e17f8d-cf37-471e-a288-72cc03a76eda

---
+++ src/qwt_plot_textlabel.h
@@ -0,0 +1,57 @@
+/* -*- mode: C++ ; c-file-style: "stroustrup" -*- *****************************
+ * Qwt Widget Library
+ * Copyright (C) 1997   Josef Wilgen
+ * Copyright (C) 2002   Uwe Rathmann
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the Qwt License, Version 1.0
+ *****************************************************************************/
+
+#ifndef QWT_PLOT_TEXT_LABEL_H
+#define QWT_PLOT_TEXT_LABEL_H 1
+
+#include "qwt_global.h"
+#include "qwt_plot_item.h"
+#include "qwt_text.h"
+
+/*!
+  \brief A plot item, which displays a text label
+
+  QwtPlotTextLabel displays a text label aligned to the plot canvas.
+
+  In opposite to QwtPlotMarker the position of the label is unrelated to
+  plot coordinates.
+    
+  As drawing a text is an expensive operation the label is cached
+  in a pixmap to speed up replots.
+
+  \sa QwtPlotMarker
+*/  
+
+class QWT_EXPORT QwtPlotTextLabel: public QwtPlotItem
+{
+public:
+    QwtPlotTextLabel();
+    virtual ~QwtPlotTextLabel();
+
+    virtual int rtti() const;
+
+    void setText( const QwtText & );
+    QwtText text() const;
+
+    void setMargin( int margin );
+    int margin() const;
+
+protected:
+    virtual void draw( QPainter *,
+        const QwtScaleMap &, const QwtScaleMap &,
+        const QRectF &) const;
+
+    void invalidateCache();
+
+private:
+    class PrivateData;
+    PrivateData *d_data;
+};
+
+#endif
