6819d0467d0b30ae9dd9fec7661729b9df54ee92
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2012-11-14 12:59:23 +0000

    workaround for Qt 4.5 added
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk@1498 29e17f8d-cf37-471e-a288-72cc03a76eda

--- qwt/examples/refreshtest/main.cpp
+++ qwt/examples/refreshtest/main.cpp
@@ -1,20 +1,21 @@
 #include "mainwindow.h"
 #include <qapplication.h>
+
 #ifndef QWT_NO_OPENGL
+#if QT_VERSION > 0x040600 && QT_VERSION < 0x050000
 #include <qgl.h>
+#endif
 #endif
 
 int main( int argc, char **argv )
 {
-#if QT_VERSION < 0x050000
-
 #ifndef QWT_NO_OPENGL
+#if QT_VERSION > 0x040600 && QT_VERSION < 0x050000
     // on my box QPaintEngine::OpenGL2 has serious problems, f.e:
     // the lines of a simple drawRect are wrong.
 
     QGL::setPreferredPaintEngine( QPaintEngine::OpenGL );
 #endif
-
 #endif
 
     QApplication a( argc, argv );
