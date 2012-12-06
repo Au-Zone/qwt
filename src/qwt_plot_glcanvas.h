13d6b6c1de13c172eab7f79a867d861fdf8bb098
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2012-12-06 14:25:25 +0000

    QFrame alike API added for QwtPlotGLCanvas
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@1562 29e17f8d-cf37-471e-a288-72cc03a76eda

--- src/qwt_plot_glcanvas.h
+++ src/qwt_plot_glcanvas.h
@@ -11,6 +11,7 @@
 #define QWT_PLOT_GLCANVAS_H
 
 #include "qwt_global.h"
+#include <qframe.h>
 #include <qgl.h>
 
 class QwtPlot;
@@ -35,13 +36,70 @@ class QwtPlot;
         type of buffers can be converted into a QImage and 
         used in combination with a regular QwtPlotCanvas.
 */
-class QWT_EXPORT QwtPlotGLCanvas : public QGLWidget
+class QWT_EXPORT QwtPlotGLCanvas: public QGLWidget
 {
     Q_OBJECT
 
+    Q_ENUMS( Shape Shadow )
+
+    Q_PROPERTY( Shadow frameShadow READ frameShadow WRITE setFrameShadow )
+    Q_PROPERTY( Shape frameShape READ frameShape WRITE setFrameShape )
+    Q_PROPERTY( double borderRadius READ borderRadius WRITE setBorderRadius )
+    Q_PROPERTY( int lineWidth READ lineWidth WRITE setLineWidth )
+    Q_PROPERTY( int midLineWidth READ midLineWidth WRITE setMidLineWidth )
+    Q_PROPERTY( int frameWidth READ frameWidth )
+
 public:
+    /*!
+        \brief Frame shadow
+
+         Unfortunately it is not possible to use QFrame::Shadow
+         as a property of a widget that is not derived from QFrame.
+         The following enum is made for the designer only. It is safe
+         to use QFrame::Shadow instead.
+     */
+    enum Shadow
+    {
+        //! QFrame::Plain
+        Plain = QFrame::Plain,
+
+        //! QFrame::Raised
+        Raised = QFrame::Raised,
+
+        //! QFrame::Sunken
+        Sunken = QFrame::Sunken
+    };
+
+    enum Shape
+    {
+        NoFrame = QFrame::NoFrame,
+
+        Box = QFrame::Box,
+        Panel = QFrame::Panel
+    };
+
     explicit QwtPlotGLCanvas( QwtPlot * = NULL );
     virtual ~QwtPlotGLCanvas();
+
+    void setFrameStyle( int style );
+    int frameStyle() const;
+
+    void setFrameShadow( Shadow );
+    Shadow frameShadow() const;
+
+    void setFrameShape( Shape );
+    Shape frameShape() const;
+
+    void setLineWidth( int );
+    int lineWidth() const;
+
+    void setMidLineWidth( int );
+    int midLineWidth() const;
+
+    int frameWidth() const;
+
+    void setBorderRadius( double );
+    double borderRadius() const;
 
     Q_INVOKABLE QPainterPath borderPath( const QRect & ) const;
 
@@ -50,6 +108,11 @@ public Q_SLOTS:
 
 protected:
     virtual void paintEvent( QPaintEvent * );
+    virtual void drawBorder( QPainter * );
+
+private:
+    class PrivateData;
+    PrivateData *d_data;
 };
 
 #endif
