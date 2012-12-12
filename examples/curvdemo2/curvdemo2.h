a7768faec8c683485d8ca84318c228bb72f89584
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2012-12-12 08:09:53 +0000

    curvdemo2 cleanup
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@1570 29e17f8d-cf37-471e-a288-72cc03a76eda

--- examples/curvdemo2/curvdemo2.h
+++ examples/curvdemo2/curvdemo2.h
@@ -1,24 +1,21 @@
-#include <qframe.h>
+#include <qwt_plot.h>
+#include <qdatetime.h>
 
-class QPainter;
 class Curve;
 
-class MainWindow: public QFrame
+class Plot: public QwtPlot
 {
 public:
-    MainWindow( QWidget * = NULL);
-    virtual ~MainWindow();
+    Plot( QWidget * = NULL);
 
 protected:
     virtual void timerEvent( QTimerEvent * );
-    virtual void paintEvent( QPaintEvent * );
 
 private:
     void updateCurves();
-    void drawCurves( QPainter *, const QRect & );
 
     enum { CurveCount = 4 };
     Curve *d_curves[CurveCount];
 
-    double d_phase;
+    QTime d_time;
 };
