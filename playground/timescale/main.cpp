970841288ad6ce13a39dc4ca15e9aeedecdb963d
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2012-12-21 16:06:26 +0000

    QwtDate completed
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@1597 29e17f8d-cf37-471e-a288-72cc03a76eda

--- playground/timescale/main.cpp
+++ playground/timescale/main.cpp
@@ -1,11 +1,33 @@
 #include <qapplication.h>
 #include "mainwindow.h"
+#include <QDateTime>
+#include <QDebug>
+#include "qwt_date.h"
+
+QString toString( QDateTime& dt )
+{
+    return dt.toString( Qt::SystemLocaleLongDate );
+}
+
+void test()
+{
+    static const QwtDate::JulianDay d1 =
+        QDateTime( QDate(1970, 1, 1) ).toUTC().date().toJulianDay();
+    static const QwtDate::JulianDay d2 = 
+        QDateTime::fromMSecsSinceEpoch( 0 ).date().toJulianDay();
+
+    qDebug() << d1 << d2;
+}
 
 int main( int argc, char **argv )
 {
     QApplication a( argc, argv );
     a.setFont( QFont( "Helvetica", 12 ) );
 
+#if 0
+test();
+exit( 0 );
+#endif
     MainWindow window;
     window.resize( 800, 600 );
     window.show();
