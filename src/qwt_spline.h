379af5297fea23adf10e3447741f0d2bf963f736
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2014-05-16 18:14:21 +0000

    splines again
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@1897 29e17f8d-cf37-471e-a288-72cc03a76eda

--- src/qwt_spline.h
+++ src/qwt_spline.h
@@ -30,33 +30,13 @@ namespace QwtSplineHarmonicMean
 
 namespace QwtSplineNatural
 {
-    // curvatures at each point
-    QWT_EXPORT QVector<double> curvatures( const QPolygonF & );
+    // derivatives at each point
+    QWT_EXPORT QVector<double> derivatives( const QPolygonF & );
 
     QWT_EXPORT QPolygonF polygon( const QPolygonF &, int numPoints );
 
     // interpolated spline as bezier curve
     QWT_EXPORT QPainterPath path( const QPolygonF & );
-
-    // calculate a, b, c
-    QWT_EXPORT void toCoefficients( 
-        double x1, double x2, double cv1,
-        double y1, double y2, double cv2, 
-        double &a, double &b, double &c );
-
-}
-
-inline void QwtSplineNatural::toCoefficients( 
-    double x1, double y1, double cv1, 
-    double x2, double y2, double cv2, 
-    double &a, double &b, double &c )
-{
-    const double dx = x2 - x1;
-    const double dy = y2 - y1;
-
-    a = ( cv2 - cv1 ) / ( 6.0 * dx );
-    b = 0.5 * cv1;
-    c = dy / dx - ( 0.5 * cv2 + cv1 ) * dx / 3.0;
 }
 
 #endif
