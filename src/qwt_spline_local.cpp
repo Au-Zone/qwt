efbf22fe8867b67220159db9bf2a5acf909937f2
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2015-10-23 15:56:38 +0000

    reorganizing local spline code
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@2330 29e17f8d-cf37-471e-a288-72cc03a76eda

--- src/qwt_spline_local.cpp
+++ src/qwt_spline_local.cpp
@@ -108,25 +108,21 @@ namespace QwtSplineLocalP
     };
 };
 
-static inline double qwtBlending(
+static inline double qwtSlopeParabolicBlending(
     double dx01, double s1, double dx12, double s2 )
 {
     return ( dx12 * s1 + dx01 * s2 ) / ( dx01 + dx12 );
 }
 
-static inline double qwtParabolicBlending( 
-    const QPointF &p0, const QPointF &p1, const QPointF &p2 )
+static inline bool qwtIsStrictlyMonotonic( double dy1, double dy2 )
 {
-    const double dx01 = p1.x() - p0.x();
-    const double s1 = ( p1.y() - p0.y() ) / dx01;
+    if ( dy1 == 0.0 || dy2 == 0.0 )
+        return false;
 
-    const double dx12 = p2.x() - p1.x();
-    const double s2 = ( p2.y() - p1.y() ) / dx12;
-
-    return qwtBlending( dx01, s1, dx12, s2 );
+    return ( dy1 > 0.0 ) == ( dy2 > 0.0 );
 }
 
-static inline double qwtAkima( double s1, double s2, double s3, double s4 )
+static inline double qwtSlopeAkima( double s1, double s2, double s3, double s4 )
 {
     if ( ( s1 == s2 ) && ( s3 == s4 ) )
     {
@@ -137,14 +133,6 @@ static inline double qwtAkima( double s1, double s2, double s3, double s4 )
     const double ds34 = qAbs( s4 - s3 );
 
     return ( s2 * ds34 + s3 * ds12 ) / ( ds12 + ds34 );
-}
-
-static inline bool qwtIsStrictlyMonotonic( double dy1, double dy2 )
-{
-    if ( dy1 == 0.0 || dy2 == 0.0 )
-        return false;
-
-    return ( dy1 > 0.0 ) == ( dy2 > 0.0 );
 }
 
 static inline double qwtSlopePChip(
@@ -181,6 +169,36 @@ static inline double qwtSlopePChip(
     const double dy2 = p3.y() - p2.y();
 
     return qwtSlopePChip( dx1, dy1, dy1 / dx1, dx2, dy2, dy2 / dx2 );
+}
+
+static inline double qwtSlopeCardinal( 
+    const QPointF &p1, const QPointF &p2, const QPointF &p3 )
+{
+    Q_UNUSED( p2 )
+    return qwtSlopeP( p1, p3 );
+}
+
+static inline double qwtSlopeParabolicBlending( 
+    const QPointF &p1, const QPointF &p2, const QPointF &p3 )
+{
+    const double dx1 = p2.x() - p1.x();
+    const double s1 = ( p2.y() - p1.y() ) / dx1;
+
+    const double dx2 = p3.x() - p2.x();
+    const double s2 = ( p3.y() - p2.y() ) / dx2;
+
+    return qwtSlopeParabolicBlending( dx1, s1, dx2, s2 );
+}
+
+static inline double qwtSlopeAkima( const QPointF &p1, const QPointF &p2, 
+    const QPointF &p3, const QPointF &p4, const QPointF &p5 )
+{
+    const double s1 = qwtSlopeP( p1, p2 );
+    const double s2 = qwtSlopeP( p2, p3 );
+    const double s3 = qwtSlopeP( p3, p4 );
+    const double s4 = qwtSlopeP( p4, p5 );
+
+    return qwtSlopeAkima( s1, s2, s3, s4 );
 }
 
 static double qwtSlopeBegin( const QwtSplineC1 *spline,
@@ -325,26 +343,26 @@ static inline void qwtSplineParabolicBlendingBoundaries(
         || ( spline->boundaryCondition() == QwtSplineC1::Periodic ) )
     {
         const QPointF pn = p[0] - ( p[n-1] - p[n-2] );
-        slopeBegin = slopeEnd = qwtParabolicBlending( pn, p[0], p[1] );
+        slopeBegin = slopeEnd = qwtSlopeParabolicBlending( pn, p[0], p[1] );
         return;
     }
 
     if ( n == 3 )
     {
         const double s0 = qwtSlopeP( p[0], p[1] );
-        const double m2 = qwtParabolicBlending( p[0], p[1], p[2] );
+        const double m2 = qwtSlopeParabolicBlending( p[0], p[1], p[2] );
 
         slopeEnd = qwtSlopeEnd( spline, points, s0, m2 );
         slopeBegin = qwtSlopeBegin( spline, points, m2, slopeEnd );
     }
     else
     {
-        const double m2 = qwtParabolicBlending( p[0], p[1], p[2] );
-        const double m3 = qwtParabolicBlending( p[1], p[2], p[3] );
+        const double m2 = qwtSlopeParabolicBlending( p[0], p[1], p[2] );
+        const double m3 = qwtSlopeParabolicBlending( p[1], p[2], p[3] );
         slopeBegin = qwtSlopeBegin( spline, points, m2, m3 );
 
-        const double mn1 = qwtParabolicBlending( p[n-4], p[n-3], p[n-2] );
-        const double mn2 = qwtParabolicBlending( p[n-3], p[n-2], p[n-1] );
+        const double mn1 = qwtSlopeParabolicBlending( p[n-4], p[n-3], p[n-2] );
+        const double mn2 = qwtSlopeParabolicBlending( p[n-3], p[n-2], p[n-1] );
         slopeEnd = qwtSlopeEnd( spline, points, mn1, mn2 );
     }
 }
@@ -376,7 +394,7 @@ static inline SplineStore qwtSplineParabolicBlending(
         const double dx12 = p[i+1].x() - p[i].x();
         const double s2 = ( p[i+1].y() - p[i].y() ) / dx12;
 
-        const double m2 = ts * qwtBlending( dx01, s1, dx12, s2 );
+        const double m2 = ts * qwtSlopeParabolicBlending( dx01, s1, dx12, s2 );
 
         store.addCubic( p[i-1], m1, p[i], m2 );
 
@@ -468,12 +486,7 @@ static inline void qwtSplineAkimaBoundaries(
         const QPointF p2 = p[0] - ( p[n-1] - p[n-2] );
         const QPointF p1 = p2 - ( p[n-2] - p[n-3] );
 
-        const double s1 = qwtSlopeP( p1, p2 );
-        const double s2 = qwtSlopeP( p2, p[0] );
-        const double s3 = qwtSlopeP( p[0], p[1] );
-        const double s4 = qwtSlopeP( p[1], p[2] );
-
-        slopeBegin = slopeEnd = qwtAkima( s1, s2, s3, s4 );
+        slopeBegin = slopeEnd = qwtSlopeAkima( p1, p2, p[0], p[1], p[2] );
 
         return;
     }
@@ -490,7 +503,7 @@ static inline void qwtSplineAkimaBoundaries(
     {
         const double s1 = qwtSlopeP( p[0], p[1] );
         const double s2 = qwtSlopeP( p[1], p[2] );
-        const double m = qwtAkima( s1, s1, s2, s2 );
+        const double m = qwtSlopeAkima( s1, s1, s2, s2 );
 
         slopeBegin = qwtSlopeBegin( spline, points, m, s2 );
         slopeEnd = qwtSlopeEnd( spline, points, slopeBegin, m );
@@ -501,8 +514,8 @@ static inline void qwtSplineAkimaBoundaries(
         const double s2 = qwtSlopeP( p[1], p[2] );
         const double s3 = qwtSlopeP( p[2], p[3] );
 
-        const double m2 = qwtAkima( s1, s1, s2, s2 );
-        const double m3 = qwtAkima( s2, s2, s3, s3 );
+        const double m2 = qwtSlopeAkima( s1, s1, s2, s2 );
+        const double m3 = qwtSlopeAkima( s2, s2, s3, s3 );
 
         slopeBegin = qwtSlopeBegin( spline, points, m2, m3 );
         slopeEnd = qwtSlopeEnd( spline, points, m2, m3 );
@@ -516,8 +529,8 @@ static inline void qwtSplineAkimaBoundaries(
             s[i] = qwtSlopeP( p[i], p[i+1] );
         }
 
-        const double m2 = qwtAkima( s[0], s[0], s[1], s[1] );
-        const double m3 = qwtAkima( s[0], s[1], s[2], s[3] );
+        const double m2 = qwtSlopeAkima( s[0], s[0], s[1], s[1] );
+        const double m3 = qwtSlopeAkima( s[0], s[1], s[2], s[3] );
 
         slopeBegin = qwtSlopeBegin( spline, points, m2, m3 );
 
@@ -527,8 +540,8 @@ static inline void qwtSplineAkimaBoundaries(
             s[i] = qwtSlopeP( p[j], p[j+1] );
         }
 
-        const double mn1 = qwtAkima( s[0], s[1], s[2], s[3] );
-        const double mn2 = qwtAkima( s[2], s[2], s[3], s[3] );
+        const double mn1 = qwtSlopeAkima( s[0], s[1], s[2], s[3] );
+        const double mn2 = qwtSlopeAkima( s[2], s[2], s[3], s[3] );
 
         slopeEnd = qwtSlopeEnd( spline, points, mn1, mn2 );
     }
@@ -559,7 +572,7 @@ static inline SplineStore qwtSplineAkima(
     {
         const double s4 = qwtSlopeP( p[i+2],  p[i+3] );
 
-        const double m2 = ts * qwtAkima( s1, s2, s3, s4 );
+        const double m2 = ts * qwtSlopeAkima( s1, s2, s3, s4 );
         store.addCubic( p[i], m1, p[i+1], m2 );
 
         s1 = s2;
@@ -569,7 +582,7 @@ static inline SplineStore qwtSplineAkima(
         m1 = m2;
     }
 
-    const double m2 = ts * qwtAkima( s1, s2, s3, slopeEnd );
+    const double m2 = ts * qwtSlopeAkima( s1, s2, s3, slopeEnd );
 
     store.addCubic( p[size - 3], m1, p[size - 2], m2 );
     store.addCubic( p[size - 2], m2, p[size - 1], ts * slopeEnd );
