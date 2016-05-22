74b8ea8fec339e9e1e72243c9c7e2c9470428b7e
rathmann <rathmann@29e17f8d-cf37-471e-a288-72cc03a76eda>
2016-05-22 17:38:51 +0000

    screenshot code added
    git-svn-id: svn://svn.code.sf.net/p/qwt/code/trunk/qwt@2457 29e17f8d-cf37-471e-a288-72cc03a76eda

--- examples/distrowatch/barchart.h
+++ examples/distrowatch/barchart.h
@@ -12,12 +12,16 @@ class BarChart: public QwtPlot
 public:
     BarChart( QWidget * = NULL );
 
+
 public Q_SLOTS:
     void setOrientation( int );
     void exportChart();
+    void doScreenShot();
 
 private:
     void populate();
+    void exportPNG( int width, int height );
+    void render( QPainter* painter, const QRectF & targetRect );
 
     DistroChartItem *d_barChartItem;
     QStringList d_distros;
